20a81f4e13acec175e25d128b1019aecd63773c9
Ceyda Turkdogan <ceyda.turkdogan@gmail.com>
2024-08-07 18:07:39 +0300

    Add project files.

---
+++ ConstructorExample/Baby.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorExample
+{
+    public class Baby
+    {
+
+        private string _name;
+        private string _surname;
+        private DateTime _birthday;
+
+
+        public Baby() {
+
+            Console.WriteLine($"Ingaaaa");
+            _birthday = DateTime.Now;
+        }
+        public Baby( string name,string surname) {
+        
+            _name = name;
+            _surname = surname;
+            Console.WriteLine($"Ingaaaa");
+            _birthday = DateTime.Now;
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthday { get; }
+
+
+        public void ShowInfo()
+        {
+
+            Console.WriteLine($"Name:{_name}\nSurname:{_surname}\nBirthday:{_birthday}");
+        }
+    }
+
+    
+}
---
+++ ConstructorExample/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using ConstructorExample;
+
+Baby baby = new Baby();
+
+baby.ShowInfo();
+
+Baby baby1 = new Baby("Ali","Veli");
+baby1.ShowInfo();
